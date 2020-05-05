5cf40219495960eed249832a7c6395a4782e39d3
xjh <xiejinghua@bluemoon.com.cn>
2020-05-05 13:09:47 +0800

    update cglib动态代理

--- src/dynamic_proxy/JGLIBProxy.java
+++ src/dynamic_proxy/JGLIBProxy.java
@@ -2,9 +2,8 @@ package dynamic_proxy;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
 
-import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author xiejh
@@ -15,14 +14,12 @@ public class JGLIBProxy {
     public static void main(String[] args) {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(Man.class);
-        enhancer.setCallback(new MethodInterceptor(){
-            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
-                System.out.println("增强");
-                return methodProxy.invokeSuper(o, args);
-            }
+        enhancer.setCallback((MethodInterceptor) (o, method, args1, methodProxy) -> {
+            System.out.println("增强");
+            return methodProxy.invokeSuper(o, args1);
         });
 
-        Man man =(Man) enhancer.create();
+        Man man = (Man) enhancer.create();
         man.say();
     }
 }
